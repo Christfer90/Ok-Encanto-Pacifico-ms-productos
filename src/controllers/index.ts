6874c57d14064f787fea48132f6b44edf57e2fb5
Christfer90 <cfpc9005@gmail.com>
2023-01-15 20:22:53 -0500

    Controladores del microservicio de productos

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,6 @@ export * from './ping.controller';
 export * from './producto-categoria.controller';
 export * from './imagen-producto.controller';
 export * from './producto-imagen.controller';
+export * from './categoria.controller';
+export * from './imagen.controller';
+export * from './producto.controller';
